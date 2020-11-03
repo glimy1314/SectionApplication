f7a4bd33525b9c7052ed4808d4e30661ba78980a
guoliang <guoliang@baiing.cn>
2020-11-03 15:17:51 +0800

    初次提交

---
+++ SecApplication/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SecApplication
+//
+//  Created by Bright on 2020/11/3.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        print("除了写代码")
+    }
+
+
+}
+
